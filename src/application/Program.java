bf6ae98c5883d4b1a9d8b1a96e3a466a6c918bc9
dc-daniel <daniel.cristianoti@hotmail.comm>
2022-10-27 22:28:02 -0300

    byID

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,21 +2,22 @@ package application;
 
 
 
-import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "books");
 		
-		Seller sl = new Seller(21,"daniel","daniel@gmail.com", new Date(),3000.00,obj);
 		
-		System.out.println(sl);
+		SellerDao sl = DaoFactory.createSellerDao();
+		
+		Seller SL = sl.findById(3);
+		
+		
+		System.out.println(SL);
         
 
 	}
