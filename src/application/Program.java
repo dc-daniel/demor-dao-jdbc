060d06a3a45262909baf9667c1db7611a6f6b784
dc-daniel <daniel.cristianoti@hotmail.comm>
2022-10-27 21:36:49 -0300

    seller antitiy class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,20 @@
 package application;
 
+
+
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "books");
-		System.out.println(obj);
+		
+		Seller sl = new Seller(21,"daniel","daniel@gmail.com", new Date(),3000.00,obj);
+		
+		System.out.println(sl);
 
 	}
 
