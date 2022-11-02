f300236ebedad2a188bad72fad9ea43e36db588f
dc-daniel <daniel.cristianoti@hotmail.comm>
2022-11-02 12:18:52 -0300

    Reusing Instatiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,17 @@
 package application;
 
-
-
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
-
+     
 	public static void main(String[] args) {
 		
 		
 		SellerDao sl = DaoFactory.createSellerDao();
 		
+		System.out.println(" ==== Test 1: Seller findById ===== ");
 		Seller SL = sl.findById(3);
 		
 		
